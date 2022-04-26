97042b3b4ef784aac4a450efcd7e4ec5b8b102b1
devwaf <devwaf@qq.com>
2022-04-26 11:08:40 +0800

    Create test.c
    Testing the branch

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib>
+
+using namespace std;
+void main()
+{
+  int i = 0;
+  printf("please input a number:\n");
+  scanf("%d",&i);
+  printf("Ok,its succuessful!");
+}
